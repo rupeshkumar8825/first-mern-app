f7e6c45b585a19562b4352c666f32d3dda5bcc3a
Rupesh Kumar <111901045@smail.iitpkd.ac.in>
2021-12-26 04:48:45 +0530

    Initial setup done for the full stack app

---
+++ backend/index.js
@@ -0,0 +1,1 @@
+// this is the backend for the notebook application 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+// in this we will render the react application 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+// using the render method for this purpose 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+    <h1>hi this is the index.js component of the react app  </h1>
+  </>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
