9e5bde0584c3105354f9d4d5b246090e9e6a2c2c
Rupesh Kumar <111901045@smail.iitpkd.ac.in>
2021-12-26 05:42:52 +0530

    server set up is done and connection to database is done

---
+++ backend/db.js
@@ -0,0 +1,21 @@
+// in this we will write the code to connect the mongodb with the server 
+const mongoose = require("mongoose");
+
+// now calling the connect function for this purpose
+// mongoose.connect("mongodb://localhost:27017/studentcombo", {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useFindAndModify: true,
+//     useUnifiedTopology: true,
+
+// }).then(() => {
+//     console.log(`connection successful for the database `)
+// }).catch((e) => {
+//     console.log(`no connection`);
+// });
+
+mongoose.connect("mongodb://localhost:27017/notebook-database").then(() => {
+    console.log("connection successfull for the notebook database");
+}).catch((e) => {
+    console.log("no connection");
+});
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
 // this is the backend for the notebook application 
+require("./db.js");
+const port = 8000;
+
+// creating the routes for the various end points of the application 
+app.get("/", (req, res) => {
+    console.log("the user has hit the root endpoint");
+    res.send("hi this is the backend developer. You have hit the root endpoint successfully");
+});
+
+
+// now creating the server to be hosted upon 
+app.listen(port, () => {
+    console.log(`litening to the port with the port no ${port}`);
+});
